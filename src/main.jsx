b46c9cfe251ba5e99fb8954fdfbd50ed199f351e
Mayank <mayank.kr0311@gmail.com>
2026-05-06 17:56:01 +0530

    Migrate from CRA to Vite - fix all build issues

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18nConfig'
+import App from './HookWatch'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </I18nextProvider>
+  </StrictMode>
+)
